364b9eddeaa83162dc9f97ced612644d321f9713
Jakub <jakubfirlejczyk@gmail.com>
2022-11-13 21:49:41 +0100

    .

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         Student student = new Student();
-        student.sortingStudents("4A");
+        student.getStudentsByClass("4A");
         student.sortedStudentsByClass();
 
 
